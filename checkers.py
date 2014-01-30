9703802946f6dce9fc85822f8491812da33ac96f
Dan Boswell <dan@amygda.la>
2014-01-30 13:35:14 -0500

    checkers: display a static checkerboard pattern.

---
+++ checkers.py
@@ -0,0 +1,21 @@
+import argparse
+import curses
+
+
+def main(scr, n):
+    maxy, maxx = scr.getmaxyx()
+    for y in range(maxy-1):
+        for x in range(maxx):
+            xx = int(x / 2)
+            board_row = int(y / n)
+            if not abs(board_row % 2 - int((xx % (2 * n)) / n)):
+                scr.addstr(y, x, chr(0x2588))
+    scr.getch()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--size', type=int, default=None)
+    args = parser.parse_args()
+    curses.wrapper(main, args.size or 5)
+    args = parser.parse_args()
