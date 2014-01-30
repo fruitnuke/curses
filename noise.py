1523355e816959ce6b98c303d747a880431f8e1d
Dan Boswell <dan@amygda.la>
2014-01-30 13:47:30 -0500

    noise: basic animation using curses.

---
+++ noise.py
@@ -0,0 +1,22 @@
+import curses
+import random
+import time
+
+
+def main(scr):
+    scr.nodelay(True)
+    while True:
+        if scr.getch() != -1:
+            break
+        my, mx = scr.getmaxyx()
+        scr.clear()
+        for y in range(my-1):
+            for x in range(mx):
+                if random.random() > 0.65:
+                    scr.addch(y, x, chr(0x2588))
+        scr.refresh()
+        time.sleep(0.2)
+
+
+if __name__ == '__main__':
+    curses.wrapper(main)
